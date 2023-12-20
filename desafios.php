155aa42023210c2274ff66aa36452659cb4b563f
Taldo SaTis <141753709+TaldoEsau@users.noreply.github.com>
2023-12-20 12:34:15 -0300

    Add files via upload

---
+++ desafios.php
@@ -0,0 +1,30 @@
+<?php
+
+/*
+ * MOSTRAR NUMEROS IMPAR ATÉ O 100
+ * for ($cont = 1; $cont < 100; $cont = $cont+2){
+    echo "$cont". PHP_EOL;
+}*/
+
+/*
+TABUADA
+
+for ($tabuada = 1; $tabuada <= 10; $tabuada ++){
+    echo PHP_EOL;
+
+        for($i = 1; $i <=10; $i++){
+        $r = $tabuada * $i;
+        echo "$tabuada x $i  = $r". PHP_EOL;
+    }
+}*/
+
+/*
+
+CALCULAR O IMC
+
+$altura = 1.74;
+$peso = 60;
+
+   $r = $peso / ($altura * $altura);
+   echo "$r";
+*/
